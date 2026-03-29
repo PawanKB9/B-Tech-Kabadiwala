8ddbd39b781b335236a020641a49e2da45a09cdb
PawanKB <bindpawan500@gmail.com>
2026-03-29 11:37:11 +0530

    fixed deployment bug

--- client/app/you/agent-policy-before-add/page.tsx
+++ client/app/you/agent-policy-before-add/page.tsx
@@ -1,11 +1,15 @@
+import { Suspense } from "react";
 import AgentPolicyBeforeAdd from "./agentPolicy";
 import AcceptButton from "./AcceptButton";
 
 export default function TermsPage() {
   return (
     <div className="max-w-5xl mx-auto p-6 border rounded-lg shadow">
-      <AgentPolicyBeforeAdd /> {/* Server-rendered content */}
-      <AcceptButton /> {/* Client logic */}
+      <AgentPolicyBeforeAdd />
+
+      <Suspense fallback={<div>Loading...</div>}>
+        <AcceptButton />
+      </Suspense>
     </div>
   );
 }
